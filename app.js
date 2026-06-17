8c4e3c6d4125d71f4bf6ad174853d496c77196ee
jaysantosh <jaysharma9321js@gmail.com>
2026-06-17 22:56:31 +0530

    added port in connection.js

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get("/", (req, resp)=>{
 })
 
 app.listen(process.env.PORT, () => {
-  console.log("🚀 Server running on port 3000");
+  console.log("🚀 Server running on port"+process.env.PORT);
 });
